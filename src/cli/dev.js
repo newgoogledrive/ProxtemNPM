3cad0aedd549dcd80c5d77a905fab8c40f349f1f
7Locking <mmttsullivan3@gmail.com>
2025-12-04 11:07:52 -0500

    Create dev.js

---
+++ src/cli/dev.js
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+import express from 'express';
+import { createProxy } from '../server/basic-proxy.js';
+import { middleware } from '../server/express-middleware.js';
+
+export function devCommand() {
+  const app = createProxy('http://localhost:3000'); // Replace with your target
+  app.use(middleware());
+
+  const port = 4000;
+  app.listen(port, () => {
+    console.log(`[Proxtem] Dev server running on http://localhost:${port}`);
+  });
+}

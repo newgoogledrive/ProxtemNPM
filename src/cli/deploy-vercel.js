72e929d3cbe5ff088592f58f99b347610ae15648
7Locking <mmttsullivan3@gmail.com>
2025-12-04 11:08:50 -0500

    Create deploy-vercel.js

---
+++ src/cli/deploy-vercel.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+export function deployVercelCommand() {
+  console.log('[Proxtem] Deploying project to Vercel...');
+  console.log('⚠️ This is a stub. Implement Vercel CLI deployment here.');
+}

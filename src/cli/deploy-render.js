559d5ad4b09eed521d5437cecb43005a6ede4d59
7Locking <mmttsullivan3@gmail.com>
2025-12-04 11:08:21 -0500

    Create deploy-render.js

---
+++ src/cli/deploy-render.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+export function deployRenderCommand() {
+  console.log('[Proxtem] Deploying project to Render...');
+  console.log('⚠️ This is a stub. Implement Render API deployment here.');
+}
